3b48a8d13535c9b767993564a7c73ca92babd921
Ashish Keshri <keshri.ashish1@gmail.com>
2023-01-17 21:29:05 +0530

    error-page

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div className='container'>Error</div>
+    <>
+    <div className="placeholder-wave bg-dark p-2 text-white bg-opacity-75" style={{ height: "100vh" }}>
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <p className="text-center display-1 text-light">404</p>
+      </div>
+    </div>
+  </>
   )
 }
 
